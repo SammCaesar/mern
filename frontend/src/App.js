58609f31bf534018f6df935f6a254919b599a044
Samm <samueliturra117@gmail.com>
2024-02-06 01:50:19 -0500

    Added more login functionality and made header chnage based on login or out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import { Container } from "react-bootstrap";
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -7,14 +9,15 @@ const App = () => {
   return (
     <>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
           <Outlet />
         </Container>
       </main>
       <Footer />
+      <ToastContainer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
