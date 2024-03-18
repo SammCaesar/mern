28809677a3ebb59db845895f4c0a61ebeb63f3c0
Samm <samueliturra117@gmail.com>
2024-03-18 00:53:01 -0400

    tried to fix other server issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => { 
-    try{
-        const conn = await mongoose.connect(process.env.MONG_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(`Error: ${error.message}`);
-        process.exit(1);
-    }
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://sam123:sam123@cluster0.wxfweow.mongodb.net/proshop?retryWrites=true&w=majority",
+    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
